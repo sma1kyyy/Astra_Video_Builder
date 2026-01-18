11dc5951986093a45c2c5c0a7b856115edb39e58
sma1kyyy <sma1kyyy001@gmail.com>
2026-01-18 22:37:23 +0300

    refactor: enhance data models for annotations and text objects

--- src/schemas/annotation_object.py
+++ src/schemas/annotation_object.py
@@ -1,18 +1,32 @@
 from src.schemas.ComponentObject import ComponentObject
 
 class AnnotationObject(ComponentObject):
+    # кирилл: wait и duration и label и text (для квадратов)
+    __static_attributes__ = [
+        "type", 
+        "transparency", 
+        "start_x", 
+        "start_y", 
+        "end_x", 
+        "end_y", 
+        "wait", 
+        "duration",
+        "label",
+        "text"
+    ]
+
     def __init__(
-            self,
-            type: str,
-            transparency: int,
-            start_x: int,
-            start_y: int,
-            end_x,
-            end_y,
-            wait = 0,
-            duration = 0,
-            label = "",
-            text = ""
+        self, 
+        type: str, 
+        transparency: float, 
+        start_x: int, 
+        start_y: int, 
+        end_x: int, 
+        end_y: int, 
+        wait: float = 0, 
+        duration: float = 0,
+        label: str = None,
+        text: str = None
     ):
         self.type = type
         self.transparency = transparency
--- src/schemas/text_object.py
+++ src/schemas/text_object.py
@@ -1,19 +1,31 @@
-from src.schemas.addable_object import AddableObject
+from src.schemas.ComponentObject import ComponentObject
 
-class TextObject(AddableObject):
+class TextObject(ComponentObject):
+    # кирилл: wait и duration в список разрешенных полей
+    __static_attributes__ = [
+        "text", 
+        "start_x", 
+        "start_y", 
+        "size", 
+        "font", 
+        "wait", 
+        "duration" 
+    ]
+
     def __init__(
-        self,
-        text: str,
-        start_x: int,
-        start_y: int,
-        size: int,
-        font="arial",
-        wait=0,
-        duration=None
+        self, 
+        text: str, 
+        start_x: int, 
+        start_y: int, 
+        size: int, 
+        font: str = "arial", 
+        wait: float = 0, 
+        duration: float = 0
     ):
-        super().__init__(wait, duration)
         self.text = text
         self.start_x = start_x
         self.start_y = start_y
         self.size = size
         self.font = font
+        self.wait = wait
+        self.duration = duration
