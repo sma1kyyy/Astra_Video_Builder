a48091cd7920f0a2dad7bc2e765c7f676c03b344
DsRase <dsrase201@gmail.com>
2026-02-11 20:58:55 +0300

    delete validation (temporary)

--- src/schemas/action_object.py
+++ src/schemas/action_object.py
@@ -12,15 +12,6 @@ class ActionObject(ComponentObject):
         point = None,
         wait = 0
     ):
-        # валидация базовых типов
-        assert isinstance(duration, int) and duration > 0, "Неверное число в duration"
-        assert isinstance(point, int) and point > 0, "Неверное число в point"
-        assert isinstance(wait, int) and wait > 0, "Неверное число в wait"
-
-        # валидация особых типов
-        assert Valid.validate_selector(selector), "Неверный формат selector"
-        assert Valid.validate_url(url), "Неверный формат URL"
-
         self.type = type
         self.url = url
         self.selector = selector
