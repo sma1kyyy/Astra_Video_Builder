c29034589cb6e7883fcc2b555fd50857fc5c45e2
sma1kyyy <sma1kyyy001@gmail.com>
2026-02-17 17:54:55 +0300

    chore: remove old files and update references after refactoring

--- src/errors/object_id_is_not_correct.py
+++
@@ -1,8 +0,0 @@
-class ObjectIdIsntValid(Exception):
-    """Исключение для обработки ситуаций, в которых в параметре scenes названия объектов неправильные."""
-    def __init__(self, attr: str | None = None):
-        """В attr передаётся название неправильной сцены."""
-        extra_info = ""
-        if attr:
-            extra_info = f"Wrong object id: {attr}"
-        super().__init__("Object ID must be a number. " + extra_info)
--- src/schemas/addable_object.py
+++
@@ -1,6 +0,0 @@
-from src.schemas.ComponentObject import ComponentObject
-
-class AddableObject(ComponentObject):
-    def __init__(self, wait=0, duration=0):
-        self.wait = wait
-        self.duration = duration
